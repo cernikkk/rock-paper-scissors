02dd25f8344f78d46bdeabd04b3ab4b3e975d799
Jan Cernik <jancernik12@gmail.com>
2022-02-05 19:24:39 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function computerPlay () {
     let randomPick = Math.floor(Math.random() * 3);
     if (randomPick === 0) return "Rock";
@@ -5,15 +6,7 @@ function computerPlay () {
     else if (randomPick === 2) return "Scissors";
 }
 
-function playerPlay (playerSelection) {
-    let trimmedSelection = playerSelection.trim();
-    let firstLetter = trimmedSelection.slice(0,1);
-    let leftText = trimmedSelection.slice(1);
-    return firstLetter.toUpperCase() + leftText.toLowerCase();
-}
-
-function playGame (computerSelection, userSelection) {
-    let playerSelection = playerPlay (userSelection);
+function playGame (computerSelection, playerSelection) {
     if (computerSelection === "Rock" && playerSelection === "Scissors") return "You lose! Rock smashes the Scissors";
     else if (computerSelection === "Paper" && playerSelection === "Rock") return "You lose! Paper wraps the Rock";
     else if (computerSelection === "Scissors" && playerSelection === "Paper") return "You lose! Scissors cuts the paper";
@@ -21,26 +14,34 @@ function playGame (computerSelection, userSelection) {
     else if (playerSelection === "Paper" && computerSelection === "Rock") return "You win! Paper wraps the Rock";
     else if (playerSelection === "Scissors" && computerSelection === "Paper") return "You win! Scissors cuts the paper";
     else if (playerSelection === computerSelection )return "It's a tie!";
-    else return "Error. Try again";
 }
 
 function game (playerSelection) {
     let roundResult = playGame(computerPlay(), playerSelection);
-    if (roundResult !== "Error. Try again" || "It's a tie!") {
+    if (roundResult !== "It's a tie!") {
         if (roundResult.slice(0,9) === "You lose!") computerCounter++;
         else playerCounter++;
     }
     else rounds--;
-    console.log(roundResult);
+    const result = document.querySelector(".result");
+    result.textContent = roundResult;
+    const playerScore = document.querySelector("#player")
+    const computerScore = document.querySelector("#computer")
+    playerScore.textContent = `Human: ${playerCounter}`;
+    computerScore.textContent = `Computer: ${computerCounter}`;
+    const finalResult = document.querySelector(".final-result");
+    if (playerCounter === 5) finalResult.textContent = "The human is the ultimate winner!";
+    else if (computerCounter === 5) finalResult.textContent = "The computer is the ultimate winner!";
 }
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      game(button.id);
+    });
+  });
+
 
 let computerCounter = 0;
 let playerCounter = 0;
 let rounds = 0;
-
-for (rounds = 0; rounds < 5; rounds++) {
-    let playerPick = prompt("Rock, Paper or Scissors?");
-    game (playerPick);
-}
-if (playerCounter > computerCounter) console.log("The human is the ultimate winner!");
-else console.log("The computer is the ultimate winner!");
